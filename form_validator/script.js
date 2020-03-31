376b41f569911289f7a83d7405979c799f12d4bb
Jan van Hest <janvanhest@outlook.com>
2020-03-31 16:21:27 +0200

    Added eventlistener. When a username is not entered the username inputfield wil have a red outline. If a username is supplied there will be a green outline around the inputfield

---
+++ form_validator/script.js
@@ -0,0 +1,30 @@
+const form = document.getElementById("form");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const password2 = document.getElementById("password2");
+
+// Show input error message.
+function showError(input, message) {
+  const formControl = input.parentElement;
+  formControl.className = "form-control error";
+  const small = formControl.querySelector("small");
+  small.innerText = message;
+}
+
+// Show succes outline
+function showSucces(input) {
+  const formControl = input.parentElement;
+  formControl.className = "form-control success";
+}
+
+// Event Listeners
+form.addEventListener("submit", function(e) {
+  e.preventDefault();
+
+  if (username.value === "") {
+    showError(username, "username is required");
+  } else {
+    showSucces(username);
+  }
+});
